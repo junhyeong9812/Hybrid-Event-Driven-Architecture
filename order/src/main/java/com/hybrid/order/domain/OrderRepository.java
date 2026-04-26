b95cace66df2e72d52b8df2d2212ecef55f6a808
junhyeong9812 <pickjog@gmail.com>
2026-04-26 16:25:37 +0900

    feat(order): add findByStatusAndCreatedAtBefore method

--- order/src/main/java/com/hybrid/order/domain/OrderRepository.java
+++ order/src/main/java/com/hybrid/order/domain/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.hybrid.order.domain;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     long countByStatus(OrderStatus status);
     List<Order> findByStatus(OrderStatus status);
+    List<Order> findByStatusAndCreatedAtBefore(OrderStatus status, Instant before);
 }
